3f73c7c6bd3432aea5303f9838163280a8304f0d
Becca <123378408+beccajoe@users.noreply.github.com>
2023-08-13 00:20:48 +0900

    Solutions: Programmers Lv. 1 JavaScript Exercises

---
+++ src/javascript/programmers/level1/descendingOrder.js
@@ -0,0 +1,21 @@
+// 정수 내림차순으로 배치하기
+
+function solution(n) {
+    var answer = 0;
+    var digits = String(n).split('');
+    digits.sort(function (a,b){ return b - a; });
+    answer = parseInt(digits.join(''));
+    return answer;
+}
+
+// 정수 내림차순으로 배치하기
+//
+// 문제 설명
+// 함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+//
+// 제한 조건
+// n은 1이상 8000000000 이하인 자연수입니다.
+
+// 입출력 예
+// n	return
+// 118372	873211
