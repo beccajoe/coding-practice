9f9b7788e5e3cf00ee463f2d3c85a448a2fb01e7
Becca <123378408+beccajoe@users.noreply.github.com>
2023-08-30 11:25:30 +0900

    Solution: Programmers Lv. 1 MySQL Exercise
    -평균 일일 대여 요금 구하기

--- src/sqlpractice/mysql/programmers/level1/select.sql
+++ src/sqlpractice/mysql/programmers/level1/select.sql
@@ -49,3 +49,7 @@ SELECT book_id, date_format(published_date, '%Y-%m-%d') from book where publishe
 -- 모든 레코드 조회하기
 -- 동물 보호소에 들어온 모든 동물의 정보를 ANIMAL_ID순으로 조회하는 SQL문을 작성해주세요. SQL을 실행하면 다음과 같이 출력되어야 합니다.
 SELECT * from animal_ins order by animal_id;
+
+-- 평균 일일 대여 요금 구하기
+-- CAR_RENTAL_COMPANY_CAR 테이블에서 자동차 종류가 'SUV'인 자동차들의 평균 일일 대여 요금을 출력하는 SQL문을 작성해주세요. 이때 평균 일일 대여 요금은 소수 첫 번째 자리에서 반올림하고, 컬럼명은 AVERAGE_FEE 로 지정해주세요.
+SELECT round(avg(daily_fee), 0) as 'AVERAGE_FEE' from car_rental_company_car where car_type = 'SUV';
