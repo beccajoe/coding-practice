288f11b55170f92906fd4bb28eb193c26b9aa866
Becca <123378408+beccajoe@users.noreply.github.com>
2023-08-31 13:31:21 +0900

    Solutions: Programmers Lv. 1 MySQL Exercise
    -과일로 만든 아이스크림 고르기

--- src/sqlpractice/mysql/programmers/level1/select.sql
+++ src/sqlpractice/mysql/programmers/level1/select.sql
@@ -53,3 +53,11 @@ SELECT * from animal_ins order by animal_id;
 -- 평균 일일 대여 요금 구하기
 -- CAR_RENTAL_COMPANY_CAR 테이블에서 자동차 종류가 'SUV'인 자동차들의 평균 일일 대여 요금을 출력하는 SQL문을 작성해주세요. 이때 평균 일일 대여 요금은 소수 첫 번째 자리에서 반올림하고, 컬럼명은 AVERAGE_FEE 로 지정해주세요.
 SELECT round(avg(daily_fee), 0) as 'AVERAGE_FEE' from car_rental_company_car where car_type = 'SUV';
+
+-- 과일로 만든 아이스크림 고르기
+-- 상반기 아이스크림 총주문량이 3,000보다 높으면서 아이스크림의 주 성분이 과일인 아이스크림의 맛을 총주문량이 큰 순서대로 조회하는 SQL 문을 작성해주세요.
+SELECT f.flavor from first_half f
+join icecream_info i on f.flavor = i.flavor
+where f.total_order > 3000 and
+i.ingredient_type = 'fruit_based'
+order by total_order desc;
