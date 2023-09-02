eebbeab42d0f957b552e4e4b5a548de884981a53
Becca <123378408+beccajoe@users.noreply.github.com>
2023-09-02 21:52:29 +0900

    Solution: Programmers Lv. 2 MySQL Exercise
    -최솟값 구하기

--- src/sqlpractice/mysql/programmers/level2/sumMaxMin.sql
+++ src/sqlpractice/mysql/programmers/level2/sumMaxMin.sql
@@ -1,3 +1,7 @@
 -- 동물 수 구하기
 -- 동물 보호소에 동물이 몇 마리 들어왔는지 조회하는 SQL 문을 작성해주세요.
 SELECT count(animal_id) from animal_ins;
+
+-- 최솟값 구하기
+-- 동물 보호소에 가장 먼저 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요.
+SELECT datetime from animal_ins order by datetime limit 1;
