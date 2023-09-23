02230eecb12c9da52270e7da7b4e7e7b005e5495
Becca <123378408+beccajoe@users.noreply.github.com>
2023-09-23 14:00:22 +0900

    Solutions: Programmers Lv. 0 Java Exercises
    -카운트 업
    -배열의 원소만큼 추가하기
    -꼬리 문자열
    -배열에서 문자열 대소문자 변환하기
    -특정한 문자를 대문자로 바꾸기

---
+++ src/javapractice/programmers/level0/Count.java
@@ -0,0 +1,30 @@
+package javapractice.programmers.level0;
+// 카운트 업
+public class Count {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[end_num - start_num + 1];
+        for (int i = 0; i < answer.length; i++){
+            if (start_num <= end_num){
+                answer[i] = start_num;
+                start_num++;
+            }
+        }
+        return answer;
+    }
+}
+
+// 카운트 업
+//
+// 문제 설명
+// 정수 start_num와 end_num가 주어질 때, start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+//
+// 제한사항
+// 0 ≤ start_num ≤ end_num ≤ 50
+//'
+// 입출력 예
+// start_num	end_num	result
+// 3	10	[3, 4, 5, 6, 7, 8, 9, 10]
+
+// 입출력 예 설명
+// 입출력 예 #1
+// 3부터 10까지의 숫자들을 담은 리스트 [3, 4, 5, 6, 7, 8, 9, 10]를 return합니다.
