076f0c23af46176aa61f4fdd21a17fedcac86147
Yeon <123378408+yeon0123@users.noreply.github.com>
2023-12-11 14:41:28 +0900

    Solutions: Programmers Lv. 0 Java Exercises
    -한 번만 등장한 문자
    -배열의 길이를 2의 거듭제곱으로 만들기
    -간단한 논리 연산

---
+++ src/javapractice/programmers/level0/Once.java
@@ -0,0 +1,48 @@
+package javapractice.programmers.level0;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+// 한 번만 등장한 문자
+public class Once {
+    public String solution(String s) {
+        Map<Character, Integer> answerMap = new HashMap<>();
+        for (char c : s.toCharArray()){
+            answerMap.put(c, answerMap.getOrDefault(c, 0) + 1);
+       }
+        StringBuilder answerBuilder = new StringBuilder();
+        for (Map.Entry<Character, Integer> entry : answerMap.entrySet()){
+            if (entry.getValue() == 1){
+                answerBuilder.append(entry.getKey());
+            }
+        }
+        char[] sortedAnswer = answerBuilder.toString().toCharArray();
+        Arrays.sort(sortedAnswer);
+        String answer = String.valueOf(sortedAnswer);
+        return answer;
+    }
+}
+// 한 번만 등장한 문자
+// 문제 설명
+// 문자열 s가 매개변수로 주어집니다. s에서 한 번만 등장하는 문자를 사전 순으로 정렬한 문자열을 return 하도록 solution 함수를 완성해보세요. 한 번만 등장하는 문자가 없을 경우 빈 문자열을 return 합니다.
+//
+// 제한사항
+// 0 < s의 길이 < 1,000
+// s는 소문자로만 이루어져 있습니다.
+//
+// 입출력 예
+// s	result
+// "abcabcadc"	"d"
+// "abdc"	"abcd"
+// "hello"	"eho"
+//
+// 입출력 예 설명
+// 입출력 예 #1
+// "abcabcadc"에서 하나만 등장하는 문자는 "d"입니다.
+//
+// 입출력 예 #2
+// "abdc"에서 모든 문자가 한 번씩 등장하므로 사전 순으로 정렬한 "abcd"를 return 합니다.
+//
+// 입출력 예 #3
+// "hello"에서 한 번씩 등장한 문자는 "heo"이고 이를 사전 순으로 정렬한 "eho"를 return 합니다.
