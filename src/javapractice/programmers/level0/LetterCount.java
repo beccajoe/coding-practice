5f6e90b318db6b88bb9e9aaf7e0963cc223b1004
Yeon <123378408+yeon0123@users.noreply.github.com>
2023-12-14 13:02:10 +0900

    Solutions: Programmers Lv. 0 Java Exercises
    -2의 영역
    -문자열 묶기
    -리스트 자르기
    -컨트롤 제트
    -7의 개수
    -이진수 더하기
    -커피 심부름
    -qr code
    -조건에 맞게 수열 변환하기 2
    -소인수분해
    -잘라서 배열로 저장하기
    -문자 개수 세기

---
+++ src/javapractice/programmers/level0/LetterCount.java
@@ -0,0 +1,44 @@
+package javapractice.programmers.level0;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 문자 개수 세기
+public class LetterCount {
+    public int[] solution(String my_string) {
+        int[] answer = new int[52];
+        Map<Character, Integer> countLetters = new HashMap<>();
+        for (char c : my_string.toCharArray()){
+            countLetters.put(c, countLetters.getOrDefault(c, 0) + 1);
+        }
+        Character currentLetter = 'A';
+        for (int i = 0; i < answer.length; i++){
+            if (countLetters.containsKey(currentLetter)){
+                int count = countLetters.get(currentLetter);
+                answer[i] = count;
+            } else {
+                answer[i] = 0;
+            }
+            if (currentLetter == 'Z'){
+                currentLetter = 'a';
+            } else {
+                currentLetter = (char) (currentLetter + 1);
+            }
+        }
+        return answer;
+    }
+}
+// 문자 개수 세기
+// 문제 설명
+// 알파벳 대소문자로만 이루어진 문자열 my_string이 주어질 때, my_string에서 'A'의 개수, my_string에서 'B'의 개수,..., my_string에서 'Z'의 개수, my_string에서 'a'의 개수, my_string에서 'b'의 개수,..., my_string에서 'z'의 개수를 순서대로 담은 길이 52의 정수 배열을 return 하는 solution 함수를 작성해 주세요.
+//
+// 제한사항
+// 1 ≤ my_string의 길이 ≤ 1,000
+//
+// 입출력 예
+// my_string	    result
+// "Programmers"	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 3, 1, 0, 0, 0, 0, 0, 0, 0]
+//
+// 입출력 예 설명
+// 입출력 예 #1
+// 예제 1번의 my_string에서 'P'가 1개, 'a'가 1개, 'e'가 1개, 'g'가 1개, 'm'이 2개, 'o'가 1개, 'r'가 3개, 's'가 1개 있으므로 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 3, 1, 0, 0, 0, 0, 0, 0, 0]를 return 합니다.
